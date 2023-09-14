92941b2f55b579b752d4eb3a397dfc4389b12698
Ruslan <RuslanShitik@yandex.by>
2023-09-15 00:07:49 +0300

    added transmission model

---
+++ server/models/Transmission.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const transmissionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+},{
+    timestamps: true
+})
+
+export default mongoose.model('Transmission', transmissionSchema)
